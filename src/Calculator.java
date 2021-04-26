dfd8a7c09a83137e94760066cfe68b49737f1fd8
Witold Brzezinski <witbrzezinski@gmail.com>
2021-04-26 18:36:37 +0200

    finished

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame {
 
+    double num,ans;
+    int calculation;
+
     private JFrame calculatorFrame;
     private JPanel calculatorJPanel;
     private JTextField calculatorTextField;
@@ -25,6 +28,7 @@ public class Calculator extends JFrame {
     private JButton zeroButton;
     private JButton commaButton;
     private JButton equalsButton;
+    private JLabel operationLabel;
 
 
     public Calculator(String title){
@@ -93,6 +97,76 @@ public class Calculator extends JFrame {
                 calculatorTextField.setText(calculatorTextField.getText() + "0");
             }
         });
+        commaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculatorTextField.setText(calculatorTextField.getText() + ".");
+            }
+        });
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               num = Double.parseDouble(calculatorTextField.getText());
+               calculation=1;
+               calculatorTextField.setText("");
+               operationLabel.setText(num + "+");
+            }
+        });
+        subtractionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                num = Double.parseDouble(calculatorTextField.getText());
+                calculation=2;
+                calculatorTextField.setText("");
+                operationLabel.setText(num + "-");
+            }
+        });
+        multiplicationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                num = Double.parseDouble(calculatorTextField.getText());
+                calculation=3;
+                calculatorTextField.setText("");
+                operationLabel.setText(num + "*");
+            }
+        });
+        divisionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                num = Double.parseDouble(calculatorTextField.getText());
+                calculation=4;
+                calculatorTextField.setText("");
+                operationLabel.setText(num + "/");
+            }
+        });
+        cButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculatorTextField.setText("");
+            }
+        });
+        arrowButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              int length = calculatorTextField.getText().length();
+              int number = calculatorTextField.getText().length()-1;
+              String store;
+
+              if(length>0){
+                  StringBuilder back = new StringBuilder(calculatorTextField.getText());
+                  back.deleteCharAt(number);
+                  store= back.toString();
+                  calculatorTextField.setText(store);
+              }
+            }
+        });
+        equalsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                arithmeticOperation();
+                operationLabel.setText("");
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -100,6 +174,26 @@ public class Calculator extends JFrame {
         calculator.setVisible(true);
     }
 
+    public void arithmeticOperation(){
+        switch (calculation){
+            case 1:
+                ans = num + Double.parseDouble(calculatorTextField.getText());
+                calculatorTextField.setText(Double.toString(ans));
+                break;
+            case 2:
+                ans = num - Double.parseDouble(calculatorTextField.getText());
+                calculatorTextField.setText(Double.toString(ans));
+                break;
+            case 3:
+                ans = num * Double.parseDouble(calculatorTextField.getText());
+                calculatorTextField.setText(Double.toString(ans));
+                break;
+            case 4:
+                ans = num / Double.parseDouble(calculatorTextField.getText());
+                calculatorTextField.setText(Double.toString(ans));
+                break;
+        }
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
